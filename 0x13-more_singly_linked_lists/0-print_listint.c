5d48625a671ca4eb68c17b7af2db068fc126c5b5
DeeXtimony <danieldamilaredaniels@gamil.com>
2023-04-04 13:36:40 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,14 +6,18 @@
  *
  * Return: number of nodes
  */
+#include "lists.h"
+
 size_t print_listint(const listint_t *h)
 {
-size_t num_nodes = 0;
-while (h != NULL)
-{
-printf("%d\n", h->n);
-num_nodes++;
-h = h->next;
-}
-return (num_nodes);
+    size_t num = 0;
+
+    while (h)
+    {
+        printf("%d\n", h->n);
+        num++;
+        h = h->next;
+    }
+
+    return (num);
 }
