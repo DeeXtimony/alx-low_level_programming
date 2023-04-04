080bb2e3d148924225f3325823c6570961d39261
DeeXtimony <danieldamilaredaniels@gamil.com>
2023-04-04 16:08:03 -0700

    100-reverse_listint.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,3 +1,5 @@
+#include "lists.h"
+
 /**
  * reverse_listint - Reverses a listint_t list.
  * @head: A pointer to the address of
@@ -7,16 +9,17 @@
  */
 listint_t *reverse_listint(listint_t **head)
 {
-listint_t *prev = NULL, *current = *head, *next_node;
-
+listint_t *further, *back;
 if (head == NULL || *head == NULL)
 return (NULL);
-
-for (; current != NULL; prev = current, current = next_node)
+behind = NULL;
+while ((*head)->next != NULL)
 {
-next_node = current->next;
-current->next = prev;
+further  = (*head)->next;
+(*head)->next = back;
+back = *head;
+*head = further;
 }
-*head = prev;
+(*head)->next = back;
 return (*head);
 }
