7540cfc6c3f38fbdf0fa3b85d7ed96c6e7cac75d
DeeXtimony <danieldamilaredaniels@gamil.com>
2023-04-05 14:20:39 -0700

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,18 +1,18 @@
 #include "lists.h"
 #include <stdio.h>
 
-size_t looped_listint_len(const listint_t *head);
+size_t switch_listint_len(const listint_t *head);
 size_t print_listint_safe(const listint_t *head);
 
 /**
- * looped_listint_len - Counts the number of unique nodes
+ * switch_listint_len - Counts the number of unique nodes
  *                      in a looped listint_t linked list.
  * @head: A pointer to the head of the listint_t to check.
  *
  * Return: If the list is not looped - 0.
  *         Otherwise - the number of unique nodes in the list.
  */
-size_t looped_listint_len(const listint_t *head)
+size_t switch_listint_len(const listint_t *head)
 {
 	const listint_t *tortoise, *hare;
 	size_t nodes = 1;
@@ -60,29 +60,29 @@ size_t looped_listint_len(const listint_t *head)
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t nodes, index = 0;
+	size_t nodes, index;
 
-	nodes = looped_listint_len(head);
+	nodes = switch_listint_len(head);
 
-	if (nodes == 0)
-	{
-		for (; head != NULL; nodes++)
-		{
-			printf("[%p] %d\n", (void *)head, head->n);
-			head = head->next;
-		}
-	}
+	switch (nodes) {
+		case 0:
+			for (index = 0; head != NULL; index++)
+			{
+				printf("[%p] %d\n", (void *)head, head->n);
+				head = head->next;
+			}
+			break;
+		default:
+			for (index = 0; index < nodes; index++)
+			{
+				printf("[%p] %d\n", (void *)head, head->n);
+				head = head->next;
+			}
 
-	else
-	{
-		for (index = 0; index < nodes; index++)
-		{
-			printf("[%p] %d\n", (void *)head, head->n);
-			head = head->next;
-		}
-
-		printf("-> [%p] %d\n", (void *)head, head->n);
+			printf("-> [%p] %d\n", (void *)head, head->n);
+			break;
 	}
 
 	return (nodes);
 }
+
