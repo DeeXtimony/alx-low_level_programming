e14f9270046fd610526cf7c47effd4812dddcf1e
DeeXtimony <danieldamilaredaniels@gamil.com>
2023-04-08 06:13:15 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,47 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <unistd.h>
 #include "main.h"
 
 /**
- * create_file - creates a file and writes text content to it
- * @file_name: pointer to the name of the file to create
- * @text_content: content to write to the file
- *
- * Description:
- *   This function creates a new file with the given name, and writes
- *   the given text content to it. If the file already exists, its
- *   contents are truncated.
- *
+ * create_file - creates a new file and writes text to it
+ * @filename: a pointer to a string containing the filename
+ * @text: a pointer to a string containing the text to write to the file
+ * This function creates a new file with the given @filename and writes
+ * the text in @text to it. If the file already exists, its contents
+ * will be truncated. If @text is NULL, an empty file will be created.
  * Return: 1 on success, -1 on failure
  */
-
-int create_file(const char *file_name, char *text_content)
+int create_file(const char *filename, char *text)
 {
-	int file_desc, written_chars = 0;
-	size_t content_length = 0;
+	int i = 0, fd;
 
-	if (file_name == NULL)
+	if (filename == NULL)
 		return (-1);
 
-	if (text_content == NULL)
-		text_content = "";
+	if (text == NULL)
+		text = "";
 
-	for (content_length = 0; text_content[content_length] != '\0';
-			content_length++)
-		;
-	file_desc = open(file_name, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	/* Count the number of characters in the text */
+	while (text[i] != '\0')
+		i++;
 
-	if (file_desc == -1)
+	/* Create or truncate the file and open it for writing */
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
 		return (-1);
 
-	written_chars = write(file_desc, text_content, content_length);
-
-	close(file_desc);
-
-	if (written_chars != content_length)
+	/* Write the text to the file */
+	if (write(fd, text, i) != i)
+	{
+		close(fd);
 		return (-1);
+	}
 
+	close(fd);
 	return (1);
 }
 
