6abf5abf70b799413fbe71866de6273f141ebe00
DeeXtimony <danieldamilaredaniels@gamil.com>
2023-04-10 15:04:59 -0700

    100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * get_endianness - return the endianness of the machine
+ *
+ * Return: 0 for big endian, 1 for little endian
+ */
+int get_endianness(void)
+{
+	int s = 1;
+
+	return (*((char *) &s) + '0');
+}
